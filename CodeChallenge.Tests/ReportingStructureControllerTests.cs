219b9559dd84d7227a4a9d652c388a98da9ada1e
derrickshriver <derrick.shriver@gmail.com>
2023-11-17 10:42:54 -0500

    Add reportingstructure endpoint, returning just the employee as a placeholder

---
+++ CodeChallenge.Tests/ReportingStructureControllerTests.cs
@@ -0,0 +1,55 @@
+
+using System.Net;
+using System.Net.Http;
+
+using CodeChallenge.Models;
+
+using CodeCodeChallenge.Tests.Integration.Extensions;
+using CodeCodeChallenge.Tests.Integration.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeCodeChallenge.Tests.Integration
+{
+    [TestClass]
+    public class ReportingStructureControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        // Attribute ClassInitialize requires this signature
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer();
+            _httpClient = _testServer.NewClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void GetByEmployeeId_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedFirstName = "John";
+            var expectedLastName = "Lennon";
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/reportingstructure/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var employee = response.DeserializeContent<Employee>();
+            Assert.AreEqual(expectedFirstName, employee.FirstName);
+            Assert.AreEqual(expectedLastName, employee.LastName);
+        }
+    }
+}
---
+++ CodeChallenge/Controllers/ReportingStructureController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CodeChallenge.Services;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/reportingstructure")]
+    public class ReportingStructureController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IEmployeeService _employeeService;
+
+        public ReportingStructureController(
+            ILogger<ReportingStructureController> logger, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("{id}", Name = "getByEmployeeId")]
+        public IActionResult GetEmployeeById(String id)
+        {
+            _logger.LogDebug($"Received reporting structure get request for '{id}'");
+
+            var employee = _employeeService.GetById(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+    }
+}
